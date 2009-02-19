6c69d79125847b419a7e11b17f6d228ac544e0a0
Sam Saffron <sam.saffron@gmail.com>
2009-02-19 12:25:53 +1100

    Added support for lazy evaluation of properties

---
+++ MediaLibrary/Helpers/Lazy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaLibrary.Helpers {
+
+    // comment me out for .net 3.5
+    internal delegate TResult Func<TResult>();  
+
+    internal class Lazy<T> {
+        
+        private Func<T> func;
+        private T result;
+        private bool hasValue;
+
+
+        public Lazy(Func<T> func) {
+            this.func = func;
+            this.hasValue = false;
+        }
+
+
+        public T Value {
+            get {
+                lock (func) {
+                    if (!this.hasValue) {
+                        this.result = this.func();
+                        this.hasValue = true;
+                    }
+                }
+                return this.result;
+            }
+        }
+    }
+}
