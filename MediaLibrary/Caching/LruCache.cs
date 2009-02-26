82ccc44282c505bbd3712bf003415cd851f13971
Sam Saffron <sam.saffron@gmail.com>
2009-02-26 17:02:18 +1100

    Implemented a cache which will be used to speed up the repository

---
+++ MediaLibrary/Caching/LruCache.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace MediaLibrary.Caching {
+    class LruCache<TKey, TValue> : IDictionary<TKey, TValue> {
+
+        public LruCache(int maxSize) {
+            // we may have one more entry temporarily
+            data = new Dictionary<TKey, TValue>(maxSize+1);
+            dataAsCollection = data;
+            this.maxSize = maxSize;
+        }
+
+        object sync = new object();
+        Dictionary<TKey, TValue> data;
+        IndexedLinkedList<TKey> lruList = new IndexedLinkedList<TKey>();
+        ICollection<KeyValuePair<TKey, TValue>> dataAsCollection;
+        int maxSize;
+
+
+        public void Add(TKey key, TValue value) {
+            if (!ContainsKey(key)) {
+                this[key] = value;
+            } else {
+                throw new ArgumentException("An attempt was made to insert a duplicate key in the cache.");
+            }
+        }
+
+        public bool ContainsKey(TKey key) {
+            return data.ContainsKey(key);
+        }
+
+        public ICollection<TKey> Keys {
+            get {
+                return data.Keys;
+            }
+        }
+
+        public bool Remove(TKey key) {
+            bool existed = data.Remove(key);
+            lruList.Remove(key);
+            return existed;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value) {
+            return data.TryGetValue(key, out value);
+        }
+
+        public ICollection<TValue> Values {
+            get { return data.Values; }
+        }
+
+        public TValue this[TKey key] {
+            get {
+                var value = data[key];
+                lruList.Remove(key);
+                lruList.Add(key);
+                return value;
+            }
+            set {
+                data[key] = value;
+                lruList.Remove(key);
+                lruList.Add(key);
+
+                if (data.Count > maxSize) {
+                    Remove(lruList.First);
+                    lruList.RemoveFirst();
+                }
+            }
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item) {
+            Add(item.Key, item.Value);
+        }
+
+        public void Clear() {
+            data.Clear();
+            lruList.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item) {
+            return dataAsCollection.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
+            dataAsCollection.CopyTo(array, arrayIndex);
+        }
+
+        public int Count {
+            get { return data.Count; }
+        }
+
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item) {
+
+            bool removed = dataAsCollection.Remove(item);
+            if (removed) {
+                lruList.Remove(item.Key);
+            }
+            return removed;
+        }
+
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+            return dataAsCollection.GetEnumerator();
+        }
+
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return ((System.Collections.IEnumerable)data).GetEnumerator();
+        }
+
+    }
+}
---
+++ MediaLibraryTests/TestCache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MediaLibrary.Caching;
+
+namespace MediaLibraryTests {
+    [TestFixture]
+    public class TestCache {
+
+        [Test]
+        public void TestSizeIsLimited() {
+            var cache = new LruCache<string, int>(10);
+
+            for (int i = 0; i < 20; i++) {
+                cache[i.ToString()] = i;
+            }
+
+            Assert.AreEqual(10, cache.Count);
+            for (int i = 10; i < 20; i++) {
+                Assert.IsTrue(cache.ContainsKey(i.ToString()));
+            }
+        }
+
+        [Test]
+        public void CacheAccessShouldBumpItToTheFront() {
+            var cache = new LruCache<int, int>(3);
+
+            cache[1] = 1;
+            cache[2] = 2;
+            cache[3] = 3;
+
+            var a = cache[1];
+            cache[4] = 4;
+
+            Assert.AreEqual(cache[1], 1);
+            Assert.AreEqual(cache[3], 3);
+            Assert.AreEqual(cache[4], 4);
+            Assert.IsFalse(cache.ContainsKey(2));
+        }
+    }
+}
